354d82d4ef09a773245b821b88fb59ed65aa9da4
Eli Meschures <meschures.eli@gmail.com>
2018-04-15 15:56:39 -0700

    clean up some code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-// import PropTypes from 'prop-types'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import httpClient from './httpClient'
-
-// import { Grid, Header, List } from 'semantic-ui-react'
 
 import {
   Container,
@@ -16,7 +13,7 @@ import NavBar from './NavBar'
 import LogIn from './views/LogIn'
 import LogOut from './views/LogOut'
 import About from './views/About'
-// import Contact from './views/Contact'
+import Contact from './views/Contact'
 import SignUp from './views/SignUp'
 import Home from './views/Home'
 import HomeHero from './views/HomeHero'
@@ -28,98 +25,98 @@ import Comments from './views/Comments'
 
 
 class App extends React.Component {
-    state = { currentUser: httpClient.getCurrentUser() }
+  state = { currentUser: httpClient.getCurrentUser() }
 
-    hideFixedMenu = () => this.setState({ fixed: false })
-    showFixedMenu = () => this.setState({ fixed: true })
+  hideFixedMenu = () => this.setState({ fixed: false })
+  showFixedMenu = () => this.setState({ fixed: true })
 
-    onLoginSuccess(user) {
-      this.setState({ currentUser: httpClient.getCurrentUser() })
+  onLoginSuccess(user) {
+    this.setState({ currentUser: httpClient.getCurrentUser() })
+  }
+
+  logOut() {
+    httpClient.logOut()
+    this.setState({ currentUser: null })
+  }   
+
+  render() {
+    const { currentUser, fixed } = this.state
+    return (
+        <Responsive className="App">
+          <Visibility once={false} onBottomPassed={this.showFixedMenu} onBottomPassedReverse={this.hideFixedMenu}>
+            <Segment inverted textAlign='center' vertical>
+              <NavBar currentUser={currentUser} fixed={fixed} />
+              <Route exact path="/" component={HomeHero} />
+            </Segment>
+            
+            <Container style={{ paddingTop: '3em' }}>
+              <Switch>
+
+                <Route path="/login" render={(props) => {
+                  return <LogIn {...props} onLoginSuccess={this.onLoginSuccess.bind(this)} />
+                }} />
+                
+                <Route path="/logout" render={(props) => {
+                  return <LogOut onLogOut={this.logOut.bind(this)} />
+                }} />
+                
+                <Route path="/signup" render={(props) => {
+                  return <SignUp {...props} onSignUpSuccess={this.onLoginSuccess.bind(this)} />
+                }} />
+
+                <Route path="/weed/:id" render={(props) => {
+                  return <Comments currentUser={this.state.currentUser} {...props} />
+                }} />
+
+                <Route path="/newWeed" render={(routeProps) => {
+                  return currentUser
+                  ? <NewWeed currentUser={currentUser} routeProps={routeProps} />
+                  : <Redirect to="/login" />
+                }} />
+
+                <Route path="/profile" render={(routeProps) =>{
+                  return <Profile currentUser={currentUser} routeProps={routeProps} />
+                }} />
+
+                <Route path="/editprofile" render={(routeProps) => {
+                  return <EditProfile currentUser={currentUser} onUpdateProfileSuccess={this.onLoginSuccess.bind(this)}  routeProps={routeProps} />
+                }} />
+                
+                <Route path="/about" component={About} />
+
+                <Route path="/weed" render={(routeProps) => {
+                  return <Weed currentUser={currentUser} routeProps={routeProps} />
+                }} />
+                
+                <Route path="/" component={Home} />
+                
+              </Switch>
+
+            </Container>
+            
+            {/* FOOTER */}
+            {/* <Segment inverted vertical style={{ padding: '5em 7em', position: 'fixed', bottom: 0, width: '100%' }}>
+              <Grid divided inverted stackable>
+                <Grid.Row>
+                  <Grid.Column width={3}>
+                  <Header inverted as='h4' content='About' />
+                  <List link inverted>
+                    <List.Item as='a'>About Us</List.Item>
+                    <List.Item as='a'>Contact Us</List.Item>
+                  </List>
+                  </Grid.Column>
+                  <Grid.Column width={7}>
+                  <Header as='h4' inverted>Wee.db.ase</Header>
+                  <p>Go green.</p>
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Segment> */}
+            {/* END FOOTER */}
+          </Visibility>
+        </Responsive>
+      );
     }
-  
-    logOut() {
-      httpClient.logOut()
-      this.setState({ currentUser: null })
-    }   
-  
-    render() {
-      const { currentUser, fixed } = this.state
-      return (
-          <Responsive className="App">
-            <Visibility once={false} onBottomPassed={this.showFixedMenu} onBottomPassedReverse={this.hideFixedMenu}>
-              <Segment inverted textAlign='center' vertical>
-                <NavBar currentUser={currentUser} fixed={fixed} />
-                <Route exact path="/" component={HomeHero} />
-              </Segment>
-              
-              <Container style={{ paddingTop: '3em' }}>
-                <Switch>
-
-                  <Route path="/login" render={(props) => {
-                    return <LogIn {...props} onLoginSuccess={this.onLoginSuccess.bind(this)} />
-                  }} />
-                  
-                  <Route path="/logout" render={(props) => {
-                    return <LogOut onLogOut={this.logOut.bind(this)} />
-                  }} />
-                  
-                  <Route path="/signup" render={(props) => {
-                    return <SignUp {...props} onSignUpSuccess={this.onLoginSuccess.bind(this)} />
-                  }} />
-
-                  <Route path="/weed/:id" render={(props) => {
-                    return <Comments currentUser={this.state.currentUser} {...props} />
-                  }} />
-
-                  <Route path="/newWeed" render={(routeProps) => {
-                    return currentUser
-                    ? <NewWeed currentUser={currentUser} routeProps={routeProps} />
-                    : <Redirect to="/login" />
-                  }} />
-
-                  <Route path="/profile" render={(routeProps) =>{
-                    return <Profile currentUser={currentUser} routeProps={routeProps} />
-                  }} />
-
-                  <Route path="/editprofile" render={(routeProps) => {
-                    return <EditProfile currentUser={currentUser} onUpdateProfileSuccess={this.onLoginSuccess.bind(this)}  routeProps={routeProps} />
-                  }} />
-                  
-                  <Route path="/about" component={About} />
-
-                  <Route path="/weed" render={(routeProps) => {
-                    return <Weed currentUser={currentUser} routeProps={routeProps} />
-                  }} />
-                  
-                  <Route path="/" component={Home} />
-                  
-                </Switch>
-
-              </Container>
-              
-              {/* FOOTER */}
-              {/* <Segment inverted vertical style={{ padding: '5em 7em', position: 'fixed', bottom: 0, width: '100%' }}>
-                <Grid divided inverted stackable>
-                  <Grid.Row>
-                    <Grid.Column width={3}>
-                    <Header inverted as='h4' content='About' />
-                    <List link inverted>
-                      <List.Item as='a'>About Us</List.Item>
-                      <List.Item as='a'>Contact Us</List.Item>
-                    </List>
-                    </Grid.Column>
-                    <Grid.Column width={7}>
-                    <Header as='h4' inverted>Wee.db.ase</Header>
-                    <p>Go green.</p>
-                    </Grid.Column>
-                  </Grid.Row>
-                </Grid>
-              </Segment> */}
-              {/* END FOOTER */}
-            </Visibility>
-          </Responsive>
-        );
-      }
-    }
+  }
 
 export default App;
